948403ebc5d82d511cd6df6b31e0a463f45434b6
RicardoF <rickyfernandez210@gmail.com>
2023-08-22 19:08:28 -0300

    product ok

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -3,24 +3,25 @@ import { productMongo } from "../manager/product/productManagerMongo.js";
 
 const router= Router()
 
-
 router.get('/',async(req,res)=>{
     try{
         const products = await productMongo.getproducts()
         if(products.length){
             res.status(200).json({mesage:"products:", products})
-        }else{res.status(200).json({mesage:"No product exists"})}
+        }else{res.status(500).json({mesage:"No product exists"})}
     }
     catch(error){res.status(500).json({error})}
 })
 
-router.get('/:id',async(req,res)=>{
-    const {id}= req.params
+router.get('/:pid',async(req,res)=>{
+    const {pid}= req.params
+    if (pid.length != 24){
+        res.status(400).json({mesage:`Use a correct Id`})
+        return}
     try{
-        const prodById= await productMongo.getproductById(id)
-        console.log(prodById.name)
-        if (prodById.name =="CastError"){
-            res.status(400).json({mesage:`No product exists whit the ID: ${id}`})
+        const prodById= await productMongo.getproductById(pid)
+        if (!prodById){
+            res.status(400).json({mesage:`No product exists whit the ID: ${pid}`}) //el object id lleva 24 caracteres o 12 si es menor da error 
         }else{
             res.status(200).json({prodById})}
     }
@@ -39,13 +40,37 @@ router.post('/',async (req,res)=>{
     catch(error){res.status(500).json({error})}
 })
 
-router.put('/:id',async(req,res)=>{
-    try{}
+router.put('/:pid',async(req,res)=>{
+    const pid= req.params.pid
+    const prod = req.body
+    if (pid.length != 24){
+        res.status(400).json({mesage:`Use a correct Id`})
+        return}
+    const prodById= await productMongo.getproductById(pid)
+    if (!prodById){
+        res.status(500).json({mesage:"No product exists"})
+        return}
+    try{
+        const mod= await productMongo.updateProduct(pid,prod)
+        if (!mod){
+            res.status(400).json({mesage:`No product exists whit the ID: ${pid}`})
+        }else{
+            res.status(200).json({mesage:"product modify"})}
+    }
     catch(error){res.status(500).json({error})}
 })
 
-router.delete('/:id',async(req,res)=>{
-    try{}
+router.delete('/:pid',async(req,res)=>{
+    const pid = req.params.pid
+    if (pid.length != 24){
+        res.status(400).json({mesage:`Use a correct Id`})
+        return}
+    try{
+        const del = await productMongo.deleteProduct(pid)
+        if (!del){
+            res.status(400).json({mesage:"the id no exists"})
+        }else {res.status(200).json({mesage:"the product was delete"})
+    }}
     catch(error){res.status(500).json({error})}
 })
 
